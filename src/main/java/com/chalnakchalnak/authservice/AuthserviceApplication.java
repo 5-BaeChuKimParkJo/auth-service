013550511595a1b8e7e4bc25ebfbc8acb1aa4b34
KorMJ <alswh456123@kyungsung.ac.kr>
2025-05-21 11:16:38 +0900

    BVFH-85 initial commit

---
+++ src/main/java/com/chalnakchalnak/authservice/AuthserviceApplication.java
@@ -0,0 +1,13 @@
+package com.chalnakchalnak.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthserviceApplication.class, args);
+    }
+
+}
